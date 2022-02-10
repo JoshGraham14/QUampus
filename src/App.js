3a938d0fb9a1e4ba9cf353efb22bf9bf43eb6752
Josh Graham <joshgraham14@gmail.com>
2022-02-09 22:43:09 -0500

    :sparkles::heavy_plus_sign: Implemented google maps API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './css/app.css'
+import QUMap from './components/QUMap'
 
 function App() {
 	return (
 		<div className='App'>
 			<h1>QUampus</h1>
+			<div className='map-container'>
+				<QUMap />
+			</div>
 		</div>
 	)
 }
---
+++ src/components/QUMap.js
@@ -0,0 +1,23 @@
+import { Map, GoogleApiWrapper } from 'google-maps-react'
+
+const mapStyles = {
+	width: '100%',
+	height: '100%',
+}
+
+export const QUMap = props => {
+	return (
+		<div>
+			<Map
+				zoom={16}
+				google={props.google}
+				style={mapStyles}
+				initialCenter={{ lat: 44.2253, lng: -76.4951 }}
+			/>
+		</div>
+	)
+}
+
+export default GoogleApiWrapper({
+	apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+})(QUMap)
