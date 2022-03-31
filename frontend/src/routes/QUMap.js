ae06b0323edf49713e84700a0eec6db591d1db4d
Josh Graham <joshgraham14@gmail.com>
2022-03-30 23:52:37 -0400

    :sparkles::construction: Map filtering is kinda working

--- frontend/src/routes/QUMap.js
+++ frontend/src/routes/QUMap.js
@@ -1,12 +1,44 @@
-// import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import GoogleMapReact from 'google-map-react'
+import MapMarker from '../components/mapMarker'
+
+import '../css/QUMap.css'
+
+import axios from 'axios'
 
 const QUMapLocation = props => {
+	const [locations, setLocations] = useState([])
+	const [selected, setSelected] = useState([])
 	const defaultLocation = props.defaultLocation
+
+	useEffect(() => {
+		axios
+			.get(`http://127.0.0.1:8000/${selected}/`)
+			.then(response => {
+				setLocations(response.data)
+			})
+			.catch(() => {
+				console.log('something messed up')
+			})
+	}, [selected])
+
+	const handleFilter = e => {
+		console.log(e.target.value)
+		setSelected(e.target.value)
+	}
 
 	return (
 		<>
-			<h2 className='title smaller'>Filter</h2>
+			<div className='drop-down-menu'>
+				<select onChange={handleFilter} defaultValue=''>
+					<option value='' disabled>
+						Filter
+					</option>
+					<option value='dining'>Dining</option>
+					<option value='lecturehall'>Lecture Halls</option>
+					<option value='residence'>Residences</option>
+				</select>
+			</div>
 			{/* Important! Always set the container height explicitly */}
 			<div style={{ height: '85vh', width: '100%' }}>
 				<GoogleMapReact
@@ -15,7 +47,18 @@ const QUMapLocation = props => {
 					}}
 					defaultCenter={defaultLocation.center}
 					defaultZoom={defaultLocation.zoom}
-				></GoogleMapReact>
+				>
+					{locations.map(location => {
+						return (
+							<MapMarker
+								key={location.url}
+								lat={location.lat}
+								lng={location.long}
+								item={location}
+							/>
+						)
+					})}
+				</GoogleMapReact>
 			</div>
 		</>
 	)
